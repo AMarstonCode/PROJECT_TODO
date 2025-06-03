7c68948f520ad5e8283f197a728aecb7465e03cb
RyanBrown423 <ryanmb9483@gmail.com>
2025-06-03 11:24:42 -0400

    handler is done

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -1,8 +1,9 @@
-export default function errorHandler(err, req, res, next) {
+export  function errorHandler(err, req, res, next) {
 if(err.name === "ValidationError")
    {res.status(400).json({ "message": err.message})}
 
 else if (err.name === "CastError"&& err.kind === "ObjectId" ) {res.status(400) .json({ "message": "Resource not found"})} 
    else { console.log(err.stack) 
     res.status(500).json({ "message": "Internal Server Error"})}
+    next()
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -23,4 +23,7 @@ app.use(express.json());
 
 connectDB()
 const PORT = process.env.PORT || 5000;
+const errorHandler = require("./middleware/errorHandler.js");
+app.use(errorHandler.errorHandler());
+
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
