2daafa824c93f7b093a3c4304d981d28ea0596ad
Eddiegurley <mgurley.prsvr@gmail.com>
2025-06-05 08:40:31 -0500

    did todo controller.js

--- backend/src/controllers/todoController.js
+++ backend/src/controllers/todoController.js
@@ -1,1 +1,16 @@
-// init commit
+const Todo = require('../models/Todo'); // Adjust the path as needed
+
+const createTodo = async (req, res, next) => {
+  const { title } = req.body;
+
+  try {
+    const newTodo = await Todo.create({ title });
+    res.status(201).json(newTodo);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  createTodo,
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 const mongoose = require("mongoose");
-
+console.log("MONGO_URI is:", process.env.MONGO_URI);
+const connect = "mongodb+srv://amarston648:nftw63EQyMD2@cluster0.8fg8blo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // to use enviroment variables access the process.env //
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(connect, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -20,10 +21,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+ 
 connectDB()
 const PORT = process.env.PORT || 5000;
-const errorHandler = require("./middleware/errorHandler.js");
-app.use(errorHandler.errorHandler());
+//const errorHandler = require("./middleware/errorHandler.js");
+//app.use(errorHandler.errorHandler());
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
