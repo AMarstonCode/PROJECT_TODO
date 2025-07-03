da0ccf99fabaf47960716314413fdfa86c0d22d9
VSurfCode <venixsurf@gmail.com>
2025-07-03 10:22:25 -0500

    corrected capital User

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.js");
+const User = require("../models/User.js");
 
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -4,4 +4,4 @@ const userSchema = new mongoose.Schema({
    email: { type: String, required: true, unique: true },
    password: { type: String, required: true }
 })
-module.exports =mongoose.model('user',userSchema);
+module.exports =mongoose.model('User',userSchema);
